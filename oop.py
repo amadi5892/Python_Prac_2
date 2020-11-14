7eb4d668f8d8c39d9359dd65e7563fcafe3ba23d
Parrish Williamson <williamsonparrish@gmail.com>
2020-11-14 17:13:09 -0500

    oop pt 3

--- oop.py
+++ oop.py
@@ -1,23 +1,116 @@
 mylist = [1,2,3]
 myset = set()
 
+# class Dog():
+
+#     # CLASS OBJECT ATTRIBUTE
+#     # SAME FOR ANY INSTANCE OF A CLASS
+#     species = 'mammal'
+
+#     def __init__(self,mybreed,name):
+        
+#         # Attributes
+#         # We take in the argument
+#         # Assign it using self.attribute_name
+#         self.breed = mybreed
+#         self.name = name
+
+#     # OPERATIONS/Actions --> Methods
+#     def bark(self,number):
+#         print("Woof! My name is {} and the number is {}".format(self.name,number))
+
+# my_dog = Dog('Lab','Frankie')
+
+# print(my_dog)
+# print(my_dog.species)
+# print(my_dog.breed)
+# print(my_dog.name)
+# print(my_dog.bark(45))
+
+
+# class Circle():
+
+#     # CLASS OBJECT ATTRIBUTE
+#     pi = 3.14
+
+#     def __init__(self,radius=1):
+
+#         self.radius = radius
+#         self.area = radius * radius * self.pi
+
+#     # METHOD
+#     def get_circumference(self):
+#         return self.radius * self.pi * 2
+
+# my_circle = Circle(30)
+
+# print(my_circle.pi)
+# print(my_circle.radius)
+# print(my_circle.area)
+# print(my_circle.get_circumference())
+
+
+
+# class Animal():
+
+#     def __init__(self):
+#         print("ANIMAL CREATED")
+
+#     def who_am_i(self):
+#         print("I am an animal")
+
+#     def eat(self):
+#         print('I am eating')
+
+# myanimal = Animal()
+# print(myanimal)
+
+# class Dog(Animal):
+
+#     def __init__(self):
+#         Animal.__init__(self)
+#         print("Dog Created")
+
+#     def bark(self):
+#         print('WoOf!!')
+
+# mydog = Dog()
+
+# print(mydog)
+
+
+
+
+
+# ---- Polymorphism ----
+
 class Dog():
 
-    def __init__(self,mybreed,name,spots):
-        
-        # Attributes
-        # We take in the argument
-        # Assign it using self.attribute_name
-        self.breed = mybreed
+    def __init__(self,name):
         self.name = name
 
-        # Expect boolean True/False
-        self.spots = spots
+    def speak(self):
+        return self.name + ' says woof!'
 
-my_dog = Dog(mybreed='Lab',name='Sammy',spots=False)
+class Cat():
 
-print(my_dog)
+    def __init__(self,name):
+        self.name = name
 
-print(my_dog.breed)
-print(my_dog.name)
-print(my_dog.spots)
+    def speak(self):
+        return self.name + ' says meow!'
+
+niko = Dog("niko")
+felix = Cat('felix')
+
+print(niko.speak())
+print(felix.speak())
+
+for pet in [niko,felix]:
+    print(type(pet))
+    print(type(pet.speak()))
+
+def pet_speak(pet):
+    print(pet.speak())
+
+pet_speak(felix)
